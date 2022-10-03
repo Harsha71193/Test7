aa9b4cffcb9226027be47ac1f11393c3fed3436a
91902 <91902@LAPTOP-FKJEGVM2>
2022-10-03 19:11:17 +0530

    First Commit

---
+++ src/Sample1/Test2.java
@@ -0,0 +1,17 @@
+package Sample1;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+	   System.out.println("Hi main method is running");	
+	   
+	   m1();
+	}
+	
+	
+	public static void m1()
+	{
+		System.out.println("hiii");
+	}
+}
